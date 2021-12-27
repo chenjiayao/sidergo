194c091b164c8d4d4ef14488a57ed0c927afacae
chenjiayao <chenjiayaooo@gmail.com>
2021-12-27 16:07:42 +0800

    fix 🐛(db/db.go): redisDB 初始化 index

--- db/db.go
+++ db/db.go
@@ -1,4 +1,44 @@
 package db
 
+import (
+	"github.com/chenjiayao/goredistraning/interface/db"
+	"github.com/chenjiayao/goredistraning/lib/dict"
+)
+
+var _ db.DB = &RedisDB{}
+
 type RedisDB struct {
+	dataset *dict.ConcurrentDict
+	index   int // 数据库 db
+}
+
+func NewDBInstance(index int) *RedisDB {
+	rd := &RedisDB{
+		dataset: dict.NewDict(128),
+		index:   index,
+	}
+	return rd
+}
+
+func (rd *RedisDB) Exec() {
+
+}
+
+////////////////
+type RedisDBs struct {
+	db      []*RedisDB
+	dbCount int
+}
+
+func NewDBs() *RedisDBs {
+	dbCount := 16
+	rds := &RedisDBs{
+		db:      make([]*RedisDB, dbCount),
+		dbCount: dbCount,
+	}
+
+	for i := 0; i < dbCount; i++ {
+		rds.db[i] = NewDBInstance(i)
+	}
+	return rds
 }
