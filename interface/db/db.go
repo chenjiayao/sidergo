5563256e0d7b47cb6215e83cbfb7cd39c0148d15
chenjiayao <chenjiayaooo@gmail.com>
2022-01-07 15:34:22 +0800

    🎨(interface/db/db.go):

--- interface/db/db.go
+++ interface/db/db.go
@@ -1,6 +1,8 @@
 package db
 
-import "github.com/chenjiayao/goredistraning/interface/response"
+import (
+	"github.com/chenjiayao/goredistraning/interface/response"
+)
 
 type DB interface {
 	Exec(cmdName string, args [][]byte) response.Response
