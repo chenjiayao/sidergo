46a01c7fdcf95549faedeaac07c9fc2fac2646eb
chenjiayao <chenjiayaooo@gmail.com>
2022-01-12 11:36:52 +0800

    ✨(redis/validate/redis_transaction_validate.go): validateExec 函数实现

--- redis/validate/redis_transaction_validate.go
+++ redis/validate/redis_transaction_validate.go
@@ -2,13 +2,25 @@ package validate
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/chenjiayao/goredistraning/interface/conn"
+	"github.com/chenjiayao/goredistraning/redis"
 )
 
 func ValidateMulti(conn conn.Conn, args [][]byte) error {
 	if conn.IsInMultiState() {
 		return errors.New("ERR MULTI calls can not be nested")
+	}
+	return nil
+}
+
+func ValidateExec(conn conn.Conn, args [][]byte) error {
+	if !conn.IsInMultiState() {
+		return errors.New("ERR EXEC without MULTI")
+	}
+	if len(args) > 0 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.Exec)
 	}
 	return nil
 }
