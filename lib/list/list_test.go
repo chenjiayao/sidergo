0b1b4f30701820755455201247470630322c6065
chenjiayao <chenjiayaooo@gmail.com>
2022-01-21 10:19:51 +0800

    🏁(lib/list/list_test.go): 添加 list 单元测试

--- lib/list/list_test.go
+++ lib/list/list_test.go
@@ -57,3 +57,103 @@ func TestList_Remove(t *testing.T) {
 		t.Errorf("l.Remove(1) = falied")
 	}
 }
+
+func TestList_GetElementByIndex(t *testing.T) {
+	l := MakeList()
+	l.InsertHead(1)
+	l.InsertHead(2)
+	l.InsertHead(3)
+	l.InsertHead(4)
+
+	v := l.GetElementByIndex(0)
+	got := v.(int)
+	if got != 4 {
+		t.Errorf("l.GetElementByIndex(0) = %d, want 4", got)
+	}
+
+	v = l.GetElementByIndex(9)
+	if v != nil {
+		t.Errorf("l.GetElementByIndex(9) = %v, want nil", got)
+	}
+
+	v = l.GetElementByIndex(3)
+	got = v.(int)
+	if got != 1 {
+		t.Errorf("l.GetElementByIndex(3) = %d, want 1", got)
+	}
+
+	v = l.GetElementByIndex(-1)
+	got = v.(int)
+	if got != 1 {
+		t.Errorf("l.GetElementByIndex(-1) = %d, want 1", got)
+	}
+
+	v = l.GetElementByIndex(-10)
+	if v != nil {
+		t.Errorf("l.GetElementByIndex(-1) = %d, want 1", got)
+	}
+}
+
+func TestList_Range(t *testing.T) {
+	l := MakeList()
+	l.InsertHead(1)
+	l.InsertHead(2)
+	l.InsertHead(3)
+	l.InsertHead(4) // 4 3 2 1
+
+	v := l.Range(0, 0)
+	v0 := v[0].(int)
+	if v0 != 4 {
+		t.Errorf(" l.Range(0, 0) = [%d], want [4]", v0)
+	}
+	//////////////
+	v = l.Range(0, 4)
+	got := make([]int, len(v))
+	for i := 0; i < len(v); i++ {
+		got[i] = v[i].(int)
+	}
+	want := []int{4, 3, 2, 1}
+	if !SliceEqual(got, want) {
+		t.Errorf("l.Range(0, 4) = %v, want %v", got, want)
+	}
+
+	////////////////
+	v = l.Range(1, 4)
+	got = make([]int, len(v))
+	for i := 0; i < len(v); i++ {
+		got[i] = v[i].(int)
+	}
+	want = []int{3, 2, 1}
+	if !SliceEqual(got, want) {
+		t.Errorf("l.Range(1, 4) = %v, want %v", got, want)
+	}
+
+	////////////////
+	v = l.Range(-1, 4)
+	got = make([]int, len(v))
+	for i := 0; i < len(v); i++ {
+		got[i] = v[i].(int)
+	}
+	want = []int{1}
+	if !SliceEqual(got, want) {
+		t.Errorf("l.Range(-1, 4) = %v, want %v", got, want)
+	}
+}
+
+func SliceEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	b = b[:len(a)]
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
