784eaa18928ac640d8a3ea1ceec797e5c433a095
chenjiayao <chenjiayaooo@gmail.com>
2022-01-29 17:39:14 +0800

    ✨(redis/validate/redis_hash_validate.go): hash 命令 validate 校验

---
+++ redis/validate/redis_hash_validate.go
@@ -0,0 +1,133 @@
+package validate
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/chenjiayao/goredistraning/interface/conn"
+	"github.com/chenjiayao/goredistraning/redis"
+)
+
+func ValidateHget(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 2 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HGET)
+	}
+
+	return nil
+}
+func ValidateHmget(conn conn.Conn, args [][]byte) error {
+
+	if len(args) < 2 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HGET)
+	}
+
+	return nil
+}
+
+func ValidateHmset(conn conn.Conn, args [][]byte) error {
+
+	if len(args) < 3 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HGET)
+	}
+
+	return nil
+}
+
+func ValidateHset(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 3 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HSET)
+	}
+
+	return nil
+}
+
+func ValidateHsetnx(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 3 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HSETNX)
+	}
+
+	return nil
+}
+
+func ValidateHdel(conn conn.Conn, args [][]byte) error {
+
+	if len(args) < 2 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HDEL)
+	}
+	return nil
+}
+
+func ValidateHexist(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 2 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HEXISTS)
+	}
+	return nil
+}
+
+func ValidateHgetall(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 1 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HGETALL)
+	}
+
+	return nil
+}
+
+func ValidateHval(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 1 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HVALS)
+	}
+
+	return nil
+}
+
+func ValidateHkeys(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 1 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HKEYS)
+	}
+
+	return nil
+}
+
+func ValidateHlen(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 1 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HLEN)
+	}
+
+	return nil
+}
+
+func ValidateHincrby(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 3 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HINCRBY)
+	}
+
+	increment := string(args[2])
+	_, err := strconv.Atoi(increment)
+	if err != nil {
+		return fmt.Errorf("(error) ERR value is not an integer or out of range")
+	}
+	return nil
+}
+
+func ValidateHincrbyfloat(conn conn.Conn, args [][]byte) error {
+
+	if len(args) != 3 {
+		return fmt.Errorf("ERR wrong number of arguments for '%s' command", redis.HINCRBYFLOAT)
+	}
+
+	increment := string(args[2])
+	_, err := strconv.ParseFloat(increment, 64)
+	if err != nil {
+		return fmt.Errorf("(error) ERR value is not an integer or out of range")
+	}
+	return nil
+}
