603083f943b9572a6ba163c136945acf3e34c94e
chenjiayao <chenjiayaooo@gmail.com>
2022-02-09 16:08:51 +0800

    🏁(lib/border/border_test.go): 🏁(lib/sortedset/sorted_set.go): 修改测试用例

--- lib/border/border_test.go
+++ lib/border/border_test.go
@@ -8,13 +8,13 @@ func TestParserBorder(t *testing.T) {
 
 	s1 := "12.3"
 	border1, _ := ParserBorder(s1)
-	if !(border1.Include && border1.Inf == 1 && border1.Value == 12.3) {
+	if !(border1.Include && border1.Inf == 0 && border1.Value == 12.3) {
 		t.Errorf("parase %s failed", s1)
 	}
 
 	s2 := "(12.3"
 	border2, _ := ParserBorder(s2)
-	if !(!border2.Include && border2.Inf == 1 && border2.Value == 12.3) {
+	if !(!border2.Include && border2.Inf == 0 && border2.Value == 12.3) {
 		t.Errorf("parase %s failed", s2)
 	}
 
@@ -24,9 +24,10 @@ func TestParserBorder(t *testing.T) {
 		t.Errorf("parase %s failed", s3)
 	}
 
+	//TODO 这里后续需要确认下 redis 对于 (-inf 的语义解释
 	s4 := "(-inf"
 	border4, _ := ParserBorder(s4)
-	if !(!border4.Include && border4.Inf == -1 && border4.Value == 0) {
+	if !(!border4.Include && border4.Inf == 0) {
 		t.Errorf("parase %s failed", s4)
 	}
 }
