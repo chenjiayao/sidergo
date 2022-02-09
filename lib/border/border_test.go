537109cc359e57c92b3adac002cee87fd824aeb3
chenjiayao <chenjiayaooo@gmail.com>
2022-02-09 15:36:54 +0800

    🏁(lib/border/border_test.go): 添加单元测试

---
+++ lib/border/border_test.go
@@ -0,0 +1,32 @@
+package border
+
+import (
+	"testing"
+)
+
+func TestParserBorder(t *testing.T) {
+
+	s1 := "12.3"
+	border1, _ := ParserBorder(s1)
+	if !(border1.Include && border1.Inf == 1 && border1.Value == 12.3) {
+		t.Errorf("parase %s failed", s1)
+	}
+
+	s2 := "(12.3"
+	border2, _ := ParserBorder(s2)
+	if !(!border2.Include && border2.Inf == 1 && border2.Value == 12.3) {
+		t.Errorf("parase %s failed", s2)
+	}
+
+	s3 := "-inf"
+	border3, _ := ParserBorder(s3)
+	if !(border3.Include && border3.Inf == -1 && border3.Value == 0) {
+		t.Errorf("parase %s failed", s3)
+	}
+
+	s4 := "(-inf"
+	border4, _ := ParserBorder(s4)
+	if !(!border4.Include && border4.Inf == -1 && border4.Value == 0) {
+		t.Errorf("parase %s failed", s4)
+	}
+}
