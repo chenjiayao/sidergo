45b7712b03694b6cb6abf56afd02bf68be1f8778
chenjiayao <chenjiayaooo@gmail.com>
2022-03-09 21:55:55 +0800

    feat: 🎸 cluster/exec.go
    集群模式下事务命令支持

--- cluster/exec.go
+++ cluster/exec.go
@@ -3,13 +3,23 @@ package cluster
 import (
 	"strings"
 
+	"github.com/chenjiayao/sidergo/interface/conn"
 	"github.com/chenjiayao/sidergo/interface/response"
 	"github.com/chenjiayao/sidergo/redis"
+	redisRequest "github.com/chenjiayao/sidergo/redis/request"
 	"github.com/chenjiayao/sidergo/redis/resp"
+	"github.com/chenjiayao/sidergo/redis/validate"
 )
 
 func init() {
 	RegisterClusterExecCommand(redis.Auth, defaultExec, nil)
+	RegisterClusterExecCommand(redis.Mget, ExecMget, validate.ValidateMGet)
+
+	//事务的命令肯定是在当前 server 中执行
+	RegisterClusterExecCommand(redis.Watch, selfServerExec, nil)
+	RegisterClusterExecCommand(redis.Multi, selfServerExec, nil)
+	RegisterClusterExecCommand(redis.Discard, selfServerExec, nil)
+	RegisterClusterExecCommand(redis.Exec, selfServerExec, nil)
 }
 
 /*
@@ -18,21 +28,31 @@ func init() {
 	2. 如果定位到的 node 正是当前节点，直接 cluster.Self.RedisServer
 	3. 如果不是当前节点，那么通过 TCP 连接转发到对应的节点
 */
-func defaultExec(cluster *Cluster, args [][]byte) response.Response {
+func defaultExec(cluster *Cluster, conn conn.Conn, args [][]byte) response.Response {
 
 	cmdName := strings.ToLower(string(args[0]))
-	ipPortPair := cluster.PickNode(cmdName)
+	ipPortPair := cluster.HashRing.Hit(cmdName)
 	if cluster.Self.IsSelf(ipPortPair) {
 		// cluster.Self.RedisServer.
 		//TODO这里应该转发给 redisServer 执行命令
+		cluster.Self.RedisServer.Exec(conn, &redisRequest.RedisRequet{
+			Args: args,
+		})
 		return nil
 	} else {
 		return nil
 	}
 }
 
+//在当前 node 上执行
+func selfServerExec(cluster *Cluster, conn conn.Conn, args [][]byte) response.Response {
+	return cluster.Self.RedisServer.Exec(conn, &redisRequest.RedisRequet{
+		Args: args,
+	})
+}
+
 // mget key1 key2 key3
-func ExecMget(cluster *Cluster, args [][]byte) response.Response {
+func ExecMget(cluster *Cluster, conn conn.Conn, args [][]byte) response.Response {
 	argsWithoutCmdName := args[1:]
 	resps := make([]response.Response, len(argsWithoutCmdName)/2)
 	for i := 0; i < len(argsWithoutCmdName); i += 2 {
@@ -41,7 +61,7 @@ func ExecMget(cluster *Cluster, args [][]byte) response.Response {
 			argsWithoutCmdName[i],
 			argsWithoutCmdName[i+1],
 		}
-		resps = append(resps, defaultExec(cluster, getCommand))
+		resps = append(resps, defaultExec(cluster, conn, getCommand))
 	}
 	return resp.MakeArrayResponse(resps)
 }
