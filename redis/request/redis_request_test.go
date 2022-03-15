b2e669fc09ef5d373955305e3daba12d4971863c
chenjiayao <chenjiayaooo@gmail.com>
2022-03-15 15:07:05 +0800

    test: 💍 redis/request/redis_request_test.go
    redisRequest 添加单元测试

--- redis/request/redis_request_test.go
+++ redis/request/redis_request_test.go
@@ -21,3 +21,38 @@ func TestRedisRequet_ToStrings(t *testing.T) {
 		t.Errorf("req.ToStrings() = %s, want : %s", got, want)
 	}
 }
+
+func TestRedisRequet_ToByte(t *testing.T) {
+	cmd := "*3\r\n$3\r\nget\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
+	want := []byte(cmd)
+
+	req := &RedisRequet{
+		CmdName: "get",
+		Args: [][]byte{
+			[]byte("key"),
+			[]byte("value"),
+		},
+	}
+	got := req.ToByte()
+	if !SliceEqual(want, got) {
+		t.Errorf("test failed")
+	}
+}
+
+func SliceEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	b = b[:len(a)]
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
