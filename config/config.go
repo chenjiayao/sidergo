1b0fb5ab629f33fc041df9a0182d376ff61e8f2e
chenjiayao <chenjiayaooo@gmail.com>
2022-03-23 12:10:06 +0800

    :wrench: (config/config.go) 修改默认配置

--- config/config.go
+++ config/config.go
@@ -41,12 +41,8 @@ func LoadDefaultConfig() {
 		Databases:      3,
 		RequirePass:    "",
 		Appendonly:     true,
-		EnableCluster:  true,
+		EnableCluster:  false,
 		AppendFilename: "./redis.aof",
-		Self:           "localhost:3101",
-		Nodes: []string{
-			"localhost:3102",
-		},
 	}
 }
 
