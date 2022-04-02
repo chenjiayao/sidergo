71cf6ece8b09384d34ff8e4638de27eb9f9f2f01
chenjiayao <chenjiayaooo@gmail.com>
2022-04-02 22:52:39 +0800

    test(lib/sortedset/skip_list_test.go): :white_check_mark: 添加单元测试

--- lib/sortedset/skip_list_test.go
+++ lib/sortedset/skip_list_test.go
@@ -1,10 +1,26 @@
 package sortedset
 
 import (
+	"log"
 	"testing"
 )
 
 func TestSkipList_insert(t *testing.T) {
 	skipList := MakeSkipList()
-	skipList.insert(10, "member1")
+	skipList.insert(36, "6")
+	skipList.insert(3, "1")
+	skipList.insert(12, "3")
+	skipList.insert(19, "4")
+	skipList.insert(8, "2")
+	skipList.insert(23, "5")
+
+	skipList.Print()
+}
+
+func TestSkipList_RandomLevel(t *testing.T) {
+	skiplist := MakeSkipList()
+	for i := 0; i < 100; i++ {
+		r := skiplist.RandomLevel()
+		log.Println(r)
+	}
 }
