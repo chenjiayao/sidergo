e6663c64715576aee457d8342a1fdcedd7f7e29b
chenjiayao <chenjiayaooo@gmail.com>
2022-04-08 17:33:38 +0800

    test(lib/sortedset/skip_list_test.go): :white_check_mark: 删除多余的单元测试

--- lib/sortedset/skip_list_test.go
+++ lib/sortedset/skip_list_test.go
@@ -16,7 +16,7 @@ func TestSkipList_insert(t *testing.T) {
 
 }
 
-func TestSkipList_RandomLevel(t *testing.T) {
+func TestSkipList_GetRank(t *testing.T) {
 	skipList := MakeSkipList()
 	skipList.insert(36, "5")
 	skipList.insert(3, "0")
@@ -29,39 +29,5 @@ func TestSkipList_RandomLevel(t *testing.T) {
 	want1 := 5
 	if got1 != int64(want1) {
 		t.Errorf("SkipList.GetRank() = %v, want %v", got1, want1)
-	}
-}
-
-func TestSkipList_GetRank(t *testing.T) {
-	type fields struct {
-		tail   *Node
-		header *Node
-		level  int
-		length int64
-	}
-	type args struct {
-		member string
-		score  float64
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   int64
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			skipList := &SkipList{
-				tail:   tt.fields.tail,
-				header: tt.fields.header,
-				level:  tt.fields.level,
-				length: tt.fields.length,
-			}
-			if got := skipList.GetRank(tt.args.member, tt.args.score); got != tt.want {
-				t.Errorf("SkipList.GetRank() = %v, want %v", got, tt.want)
-			}
-		})
 	}
 }
